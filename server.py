f2c03ebd2cd85a765826f602ce2665b50f0276fe
GlenCaku <glencaku@gmail.com>
2023-04-16 21:50:53 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,55 @@
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account_balance = 0
+        
+
+
+    def make_deposit(self, amount):	
+        self.account_balance += amount	
+        return self
+
+
+    def make_withdrawal(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+        else:
+            print("Insufficient funds")
+        return self
+
+
+
+    def display_user_balance(self):
+            return(f"User: {self.name}, Balance: {self.account_balance}")
+            
+
+
+    def transfer_money(self, other_user, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            other_user.account_balance += amount
+            print(f"{self.name} transferred {amount} to {other_user.name}")
+        else:
+            print("Insufficient funds")
+
+
+Glen = User("Glen Caku", 5000)
+Rashford = User("Marcus Rashford", 1000)
+Sancho = User("Jadon Sancho", 500)
+
+Glen.make_deposit(1000).make_deposit(1500).make_deposit(2000).make_withdrawal(500)
+print(Glen.display_user_balance())
+
+
+
+Rashford.make_deposit(500).make_deposit(1000).make_withdrawal(200).make_withdrawal(600)
+print(Rashford.display_user_balance())
+
+Sancho.make_deposit(1000).make_withdrawal(200).make_withdrawal(300).make_withdrawal(400)
+print(Sancho.display_user_balance())
+
+Glen.transfer_money(Sancho, 250)
+
+print(Glen.display_user_balance())
+print(Sancho.display_user_balance())
